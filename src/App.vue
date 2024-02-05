d1dfd813b2caa061fd566df811b8dfafde0bdff6
이운린 <105215297+likegitman@users.noreply.github.com>
2024-02-06 01:25:54 +0900

    Add img and modal

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,34 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="black-bg" v-if="isModal == true" @click="isModal = false">
+    <div class="white-bg">
+      <h4>상세페이지임</h4>
+      <p>상세페이지 내용임</p>
+    </div>
+  </div>
   <div class="menu">
     <a v-for="(menu, idx) in menues" :key="idx">{{ menu }}</a>
   </div>
-  <div v-for="(product, idx) in products" :key="idx">
-    <h4>{{ product }}</h4>
+  <div>
+    <img src="./assets//해달.jpg" class="room-img" />
+    <h4 @click="isModal = true">{{ products[0] }}</h4>
     <p>50만원</p>
-    <button v-on::click="reportCnt++">허위매물 신고 버튼</button
-    ><spam>신고 수: {{ reportCnt }}</spam>
-    <button v-on::click="increase">허위매물 신고 버튼</button
-    ><spam>신고 수: {{ reportCnt }}</spam>
-    <button @click="reportCnt++">허위매물 신고 버튼</button
-    ><spam>신고 수: {{ reportCnt }}</spam>
+    <button @click="reportCnt[0]++">허위매물 신고 버튼</button
+    ><spam>신고 수: {{ reportCnt[0] }}</spam>
+  </div>
+  <div>
+    <img src="./assets/해달.jpg" class="room-img" />
+    <h4 @click="isModal = true">{{ products[1] }}</h4>
+    <p>50만원</p>
+    <button @click="reportCnt[1]++">허위매물 신고 버튼</button
+    ><spam>신고 수: {{ reportCnt[1] }}</spam>
+  </div>
+  <div>
+    <img src="./assets/해달.jpg" class="room-img" />
+    <h4 @click="isModal = true">{{ products[2] }}</h4>
+    <p>50만원</p>
+    <button @click="reportCnt[2]++">허위매물 신고 버튼</button
+    ><spam>신고 수: {{ reportCnt[2] }}</spam>
   </div>
 </template>
 
@@ -21,9 +37,10 @@ export default {
   name: "App",
   data() {
     return {
-      reportCnt: 0,
+      isModal: false,
+      reportCnt: [0, 0, 0],
       menues: ["Home", "Product", "About"],
-      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],   
     };
   },
   methods: {
@@ -38,13 +55,33 @@ export default {
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 
 .menu {
@@ -57,5 +94,10 @@ export default {
   color: white;
   padding-left: 10px;
   cursor: pointer;
+}
+
+.room-img {
+  width: 100%;
+  margin-top: 40px;
 }
 </style>
